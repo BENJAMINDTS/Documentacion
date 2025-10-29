30b5820f93d2b0c665a38463c737b6767b9c61ed
BENJAMINDTS <benjamin.pk02@gmail.com>
2025-10-29 16:01:30 +0100

    feat: añadir documentación HTML Javadoc completa
    - Estructura HTML profesional generada con Javadoc
    - Navegación completa con índices y búsqueda
    - Clase App documentada: factorial/App.html
    - Archivos: index.html, help-doc.html, search.html
    - Accesibilidad web universal compatible con todos los navegadores

---
+++ 01-Documentacion_HTML/member-search-index.js
@@ -0,0 +1,1 @@
+memberSearchIndex = [{"p":"factorial","c":"App","l":"App()","u":"%3Cinit%3E()"},{"p":"factorial","c":"App","l":"factorial(int)"},{"p":"factorial","c":"App","l":"main(String[])","u":"main(java.lang.String[])"}];updateSearchResults();
